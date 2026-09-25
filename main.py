0329327589d036e789ce84baf58d491dbfefd3a5
Sarthak Gawade <sarthakgawade543@gmail.com>
2026-09-25 14:24:58 +0530

    Add Boto3 AWS connection test

---
+++ main.py
@@ -0,0 +1,14 @@
+import boto3
+
+
+def check_aws_connection():
+    sts = boto3.client("sts")
+    identity = sts.get_caller_identity()
+
+    print("AWS connection successful!")
+    print(f"Account: {identity['Account']}")
+    print(f"Identity: {identity['Arn']}")
+
+
+if __name__ == "__main__":
+    check_aws_connection()
